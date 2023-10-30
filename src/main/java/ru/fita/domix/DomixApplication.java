5bcc3dd8ce6ed416ea60a826c1e2a12a7eceee9a
Ildar <wilidon@bk.ru>
2023-10-30 21:36:48 +0400

    Initial commit

---
+++ src/main/java/ru/fita/domix/DomixApplication.java
@@ -0,0 +1,12 @@
+package ru.fita.domix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomixApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DomixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/fita/domix/DomixApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fita.domix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
