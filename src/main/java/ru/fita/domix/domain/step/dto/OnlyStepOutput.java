61b9d44a0d93227819a4b74599593503c058b43e
Ildar <wilidon@bk.ru>
2023-11-03 23:03:48 +0400

    Подрефакторил нейминги. Добавилены новые методы

--- src/main/java/ru/fita/domix/domain/step/dto/OnlyStepOutput.java
+++ src/main/java/ru/fita/domix/domain/step/dto/OnlyStepOutput.java
@@ -1,12 +1,10 @@
 package ru.fita.domix.domain.step.dto;
 
-
-import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
-
 @Data
-public class StepName {
-    @NotBlank
+public class OnlyStepOutput {
+    private long id;
     private String title;
+    private boolean multipleSelect;
 }
