186382fb5755b67919b5d7a9ab69be1f55b07040
Ildar <wilidon@bk.ru>
2023-11-03 17:48:46 +0400

    fix

--- src/main/java/ru/fita/domix/domain/calculator/dto/CalculatorOutput.java
+++ src/main/java/ru/fita/domix/domain/calculator/dto/CalculatorOutput.java
@@ -2,10 +2,14 @@ package ru.fita.domix.domain.calculator.dto;
 
 import lombok.Data;
 import ru.fita.domix.data.model.CalculatorStatus;
+import ru.fita.domix.domain.step.dto.StepOutput;
+
+import java.util.Set;
 
 @Data
 public class CalculatorOutput {
     private long id;
     private String name;
     private CalculatorStatus status;
+    private Set<StepOutput> steps;
 }
